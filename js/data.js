d7c06d2255f106888f62abdab4928b7b1bedde58
Marc Hauschildt <marc.hauschildt@gmail.com>
2019-08-05 10:43:11 -0500

    Data.js constructs People objects from staff_data.txt file

---
+++ js/data.js
@@ -0,0 +1,36 @@
+function Person(person) {
+    this.firstName = person.firstname;
+    this.lastName = person.lastname;
+    this.active = person.activeemployee;
+    this.phone = person.phonenumber;
+    this.email = person.emailaddress;
+    this.building = person.building;
+    this.jobCategory = person.jobcategory;
+    this.responsibilities = person.responsibilities;
+    this.order = person.order;
+    this.picture = person.picture;
+    this.websiteLabel = person.website;
+    this.link = person.websitelink;
+
+    Person.all.push(this);
+}
+Person.all = [];
+
+var reader = new XMLHttpRequest() || new ActiveXObject('MSXML2.XMLHTTP');
+
+function loadFile() {
+    reader.open('get', 'js/staff_data.txt', true); 
+    reader.onreadystatechange = displayContents;
+    reader.send(null);
+}
+
+function displayContents() {
+    if(reader.readyState==4) {
+        var data = JSON.parse(reader.responseText);
+        data.forEach(function(item) {
+            new Person(item);
+        });
+    }
+}
+
+loadFile();
